39aa4e9494e306564db23a51f56805f5c3aad417
Ville Mäkynen <ville.makynen@gmail.com>
2022-12-05 13:29:47 +0200

    20 sleeps 'til Christmas

---
+++ day5/prob2.py
@@ -0,0 +1,30 @@
+import fileinput
+
+lines = list(fileinput.input())
+
+for i in range(0, len(lines)):
+    if lines[i].startswith(' 1 '):
+        stacks_count = len(lines[i].split())
+        stack_lines = lines[:i]
+        op_lines = lines[i + 2:]
+        break
+
+stacks = [[] for _ in range(0, stacks_count)]
+
+for line in reversed(stack_lines):
+    for i in range(0, stacks_count):
+        label = line[1 + i * 4]
+        if len(label.strip()) > 0:
+            stacks[i].append(label)
+
+for line in op_lines:
+    parts = line.split()
+    count = int(parts[1])
+    src = int(parts[3]) - 1
+    dst = int(parts[5]) - 1
+
+    stacks[dst].extend(stacks[src][-count:])
+    del stacks[src][-count:]
+
+print(''.join([stack[-1] if len(stack) > 0 else '' for stack in stacks]))
+
